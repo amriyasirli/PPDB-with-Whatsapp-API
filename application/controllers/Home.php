e0f159b2a20c99137681c25b88a5294d15bf169f
amriyasirli <amriyasirli62@gmail.com>
2021-06-16 23:45:44 +0700

    integrasi templates medilab

---
+++ application/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('templates/header');
+        $this->load->view('templates/topbar');
+        $this->load->view('home');
+        $this->load->view('templates/footer');
+	}
+}
---
+++ application/views/templates/topbar.php
@@ -0,0 +1,62 @@
+
+<body>
+
+<!-- ======= Top Bar ======= -->
+<div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+  <div class="container d-flex">
+    <div class="contact-info mr-auto">
+      <i class="icofont-envelope"></i> <a href="mailto:contact@example.com">contact@example.com</a>
+      <i class="icofont-phone"></i> +1 5589 55488 55
+      <i class="icofont-google-map"></i> A108 Adam Street, NY
+    </div>
+    <div class="social-links">
+      <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+      <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+      <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+      <a href="#" class="skype"><i class="icofont-skype"></i></a>
+      <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+    </div>
+  </div>
+</div>
+
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
+  <div class="container d-flex align-items-center">
+
+    <h1 class="logo mr-auto"><a href="index.html">Medilab</a></h1>
+    <!-- Uncomment below if you prefer to use an image logo -->
+    <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+    <nav class="nav-menu d-none d-lg-block">
+      <ul>
+        <li class="active"><a href="">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#departments">Departments</a></li>
+        <li><a href="#doctors">Doctors</a></li>
+        <li class="drop-down"><a href="">Drop Down</a>
+          <ul>
+            <li><a href="#">Drop Down 1</a></li>
+            <li class="drop-down"><a href="#">Deep Drop Down</a>
+              <ul>
+                <li><a href="#">Deep Drop Down 1</a></li>
+                <li><a href="#">Deep Drop Down 2</a></li>
+                <li><a href="#">Deep Drop Down 3</a></li>
+                <li><a href="#">Deep Drop Down 4</a></li>
+                <li><a href="#">Deep Drop Down 5</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Drop Down 2</a></li>
+            <li><a href="#">Drop Down 3</a></li>
+            <li><a href="#">Drop Down 4</a></li>
+          </ul>
+        </li>
+        <li><a href="#contact">Contact</a></li>
+
+      </ul>
+    </nav><!-- .nav-menu -->
+
+    <a href="#appointment" class="appointment-btn scrollto">Make an Appointment</a>
+
+  </div>
+</header><!-- End Header -->
